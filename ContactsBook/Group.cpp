1340a8554a0f866343a676001fe48d6b2d4202ff
2112Alexander2112 <sumerok16@gmail.com>
2023-11-25 13:22:54 +0200

    add Book && creating constructors Group

--- ContactsBook/Group.cpp
+++ ContactsBook/Group.cpp
@@ -1,2 +1,47 @@
 #include "framework.h"
 #include "Group.h"
+#include <vector>
+
+Group::Group()
+	: name("--"), size(20), count(0)
+{
+	contacts[size] = {};
+}
+
+Group::Group(std::string name, int size)
+	: name(name), size(20), count(0)
+{
+	contacts[size] = {};
+}
+
+Group::~Group()
+{
+}
+
+std::string Group::getName() const
+{
+	return name;
+}
+
+int Group::getSize() const
+{
+	return size;
+}
+
+int Group::getCount() const
+{
+	return count;
+}
+
+Contact* Group::getContacts() const
+{
+	return contacts;
+}
+
+void Group::addContact(const Contact& c)
+{
+}
+
+void Group::delContact(std::string name)
+{
+}
